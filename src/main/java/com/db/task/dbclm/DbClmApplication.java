cbb323901118e00fa17a864819fb5b2bad074c0b
Nagaraja Ramappa <nagaraja.rbs@gmail.com>
2021-11-06 11:51:51 +0100

    initial project setup

---
+++ src/main/java/com/db/task/dbclm/DbClmApplication.java
@@ -0,0 +1,13 @@
+package com.db.task.dbclm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbClmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbClmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/task/dbclm/DbClmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.task.dbclm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbClmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
