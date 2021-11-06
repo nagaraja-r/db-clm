3bd250c0af7b576a8f6e3a1b7fa053b8765a15e9
Nagaraja Ramappa <nagaraja.rbs@gmail.com>
2021-11-06 14:27:27 +0100

    designed the APIs and OPEN API integration

---
+++ src/main/java/com/db/task/dbclm/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.db.task.dbclm.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("Contact DB ClM API").description(
+                        "This is a db-clm service to manage the NACE Details"));
+    }
+}
---
+++ src/main/java/com/db/task/dbclm/dto/NomenclatureEconomicActivity.java
@@ -0,0 +1,23 @@
+package com.db.task.dbclm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class NomenclatureEconomicActivity implements Serializable {
+    private Long order;
+    private Integer level;
+    private String code;
+    private String parent;
+    private String description;
+    private String itemIncludes;
+    private String itemAlsoIncludes;
+    private String ruling;
+    private String itemExcludes;
+    private String referenceToIsic;
+}
